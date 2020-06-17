69e28a40d98ac8c4dcbe1725d50745ba5b4f0548
Guillermo Alfaro <galfarotolon@gmail.com>
2020-06-17 11:18:02 -0500

    init changes

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,24 @@
 const { add } = require('./calculator.js');
 
 // test away!
+describe('calculator', function () {
+    it('should run tests using it()', function () {
+
+        //setup - arrange
+
+
+        //execution of SUT (system under test) - act
+
+
+
+        //assertions - assert
+
+
+    })
+
+    describe('add()', function () {
+
+
+
+    })
+})
